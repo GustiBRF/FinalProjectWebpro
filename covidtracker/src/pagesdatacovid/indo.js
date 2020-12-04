0e23baa2071966415ae31b65b313f54699402168
GustiBRF <gustibagusrf@gmail.com>
2020-12-04 14:58:46 -0700

    #6 Sukses tampilkan Data API Indonesia

--- covidtracker/src/pagesdatacovid/indo.js
+++ covidtracker/src/pagesdatacovid/indo.js
@@ -4,32 +4,34 @@ import axios from "axios";
 
 
 const Indo = () => {
-    const [users, setUsers] = useState([])
-
+    const [jumkas, setJum] = useState([])
+    const [meninggal, setMen] = useState([])
+    const [per, setPer] = useState([])
+    const [sembuh, setSem] = useState([])
     useEffect(()  => {
           axios
           .get("https://indonesia-covid-19.mathdro.id/api")
-          .then((response) => setUsers(response));
+          .then((response) => {
+            setJum(response.data.jumlahKasus)
+            setMen(response.data.meninggal)
+            setPer(response.data.perawatan)
+            setSem(response.data.sembuh)
+          });
 
     }, []);
 
-    console.log(users);
 
     return (
-        <h1>INDO PAGES</h1>
-        // <>
-        //     {
-        //         users.map((item) => {
-        //             return (
-        //                 <Layout
-        //                 confirmed={item.confirmed}
-        //                 deaths={item.deaths}
-        //                 recovered={item.recovered}
-        //                 />
-        //             );
-        //         })
-        //     }
-        // </>
+            
+        <div>
+            <h1>Data Covid Indonesia</h1>
+             <p>Jumlah Kasus: {jumkas}</p>
+             <p>Meninggal : {meninggal}</p>
+             <p>Dalam Perawatan : {per}</p>
+             <p>Total Sembuh : {sembuh}</p>
+        </div>
+
+    
     );
 };
 
